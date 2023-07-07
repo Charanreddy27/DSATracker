edcaf7f96fd2f13c188a6f4da17493925a3d0f0c
Charan <charanbapireddy@gmail.com>
2023-07-07 15:59:00 +0530

    Searchsort-question13

--- src/450DSAFinal.js
+++ src/450DSAFinal.js
@@ -822,6 +822,14 @@ const data = [
 				Notes: "",
 				URL: "https://practice.geeksforgeeks.org/problems/stickler-theif/0",
 			},
+			{
+				Topic: "Searching & Sorting",
+				Problem: "Count triplet with sum smaller than a given value",
+				Done: false,
+				Bookmark: false,
+				Notes: "",
+				URL: "https://practice.geeksforgeeks.org/problems/count-triplets-with-sum-smaller-than-x5549/1",
+			},
 		]
 	}
 ]
