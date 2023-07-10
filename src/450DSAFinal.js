d53ae2999fb1e79069bc318d48378a8bc1a5c683
Charan <charanbapireddy@gmail.com>
2023-07-10 23:32:01 +0530

    arrays-question1

--- src/450DSAFinal.js
+++ src/450DSAFinal.js
@@ -4,6 +4,15 @@ const data = [
 		position: 0,
 		started: false,
 		doneQuestions: 0,
-		questions: []
+		questions: [
+			{
+				Topic: "Array",
+				Problem: "Reverse the array",
+				Done: false,
+				Bookmark: false,
+				Notes: "",
+				URL: "https://practice.geeksforgeeks.org/problems/reverse-an-array/0",
+			}
+		]
 	}
 ]
