42426a6e444010f58abc0baa3eea09fe1b9bd594
Charan <charanbapireddy@gmail.com>
2023-07-10 23:32:36 +0530

    arrays-question2

--- src/450DSAFinal.js
+++ src/450DSAFinal.js
@@ -12,7 +12,15 @@ const data = [
 				Bookmark: false,
 				Notes: "",
 				URL: "https://practice.geeksforgeeks.org/problems/reverse-an-array/0",
-			}
+			},
+			{
+				Topic: "Array",
+				Problem: "Find the maximum and minimum element in an array",
+				Done: false,
+				Bookmark: false,
+				Notes: "",
+				URL: "https://practice.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1",
+			},
 		]
 	}
 ]
